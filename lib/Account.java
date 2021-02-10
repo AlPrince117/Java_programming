dd2b7144a39331e5b73769b8530816a0da78e528
pwilson2021 <philip.yirenkyi@turntabl.io>
2021-02-10 05:09:27 +0000

    Solved 4 questions in arrays

--- lib/Account.java
+++ lib/Account.java
@@ -1,12 +1,27 @@
 
 public class Account {
     double total_price = 0;
+    protected Trade[] trades;
 
-    public double getTotalPrice(){
+    public double getTotalPrice() {
         return this.total_price;
     }
 
     public void setTotalPrice(double value) {
         this.total_price += value;
+    }
+
+    public void printTrades() {
+        for (Trade trade : this.trades) {
+            trade.toString();
+        }
+    }
+
+    public void findTradesWithPriceLessThan10() {
+        for (Trade trade : this.trades) {
+            if (trade.price < 10.00) {
+                trade.toString();
+            }
+        }
     }
 }
